a8cecf23abe6f21b3a73e61076097ba02fede4bd
Bhushan Bire <53039678+Bhushanb55@users.noreply.github.com>
2021-06-08 00:29:01 +0530

    Delete Scoping.js

--- 5. ES6 and TYPESCRIPT/Scoping.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function forScope() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i); //prints the values 0 through 4;
-    }
-    console.log(i); //prints 5;
-}
-forScope();
