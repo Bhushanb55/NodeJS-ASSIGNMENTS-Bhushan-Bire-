30d8f40bc5e87caa851f1ef344757702212bc59d
Bhushan Bire <53039678+Bhushanb55@users.noreply.github.com>
2021-06-08 00:28:59 +0530

    Delete Template Literals.js

--- 5. ES6 and TYPESCRIPT/Template Literals.js
+++
@@ -1,6 +0,0 @@
-let laptop_model = "Asus VivoBook";
-let desk_no = "515";
-let name = "Bhushan Bire";
-
-console.log( `I am ${name} have some issue with
-${laptop_model} and my desk number is ${desk_no}.`);
